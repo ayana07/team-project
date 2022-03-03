c75e54321b3b750acc2b8d94f47f1def84d92eab
Aizhamal Sulaimanova <aijamal.sulaimanova.kk.@gmail.com>
2022-03-03 14:34:20 +0600

    added search

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -1,10 +1,11 @@
+import { Margin } from "@mui/icons-material";
 import { Grid, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useProducts } from "../../contexts/PoductContext";
 import MediaCard from "../Product/ProductCard";
 import SideBar from "../SideBar/SideBar";
-import { useParams } from "react-router-dom";
+import "./ProductStyles/ProductList.css";
 
 const ProductList = ({ products }) => {
 	const { getProducts } = useProducts();
@@ -38,36 +39,38 @@ const ProductList = ({ products }) => {
 	}, [search]);
 
 	return (
-		<div style={{ display: "flex", backgroundColor: "#000" }}>
-			<div className="side-bar">
-				<SideBar />
+		<>
+			<div className="text-field">
+				<TextField
+					color="secondary"
+					label="Search"
+					variant="standard"
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
 			</div>
-			<TextField
-				className="text-field"
-				color="warning"
-				label="Search"
-				variant="standard"
-				sx={{ margin: "2vw", width: "10vw" }}
-				value={search}
-				onChange={(e) => setSearch(e.target.value)}
-			/>
-			<div className="blog-left">
-				<Grid container spacing={6}>
-					{products ? (
-						products.map((item) => (
-							<Grid item>
-								{/* <ProductCard item={item} key={item.id} /> */}
-								<MediaCard item={item} key={item.id} />
-							</Grid>
-						))
-					) : (
-						<>
-							<h2>..Loading</h2>
-						</>
-					)}
-				</Grid>
+			<div style={{ display: "flex", backgroundColor: "#000" }}>
+				<div className="side-bar">
+					<SideBar />
+				</div>
+
+				<div className="blog-left">
+					<Grid container spacing={6}>
+						{products ? (
+							products.map((item) => (
+								<Grid item>
+									<MediaCard item={item} key={item.id} />
+								</Grid>
+							))
+						) : (
+							<>
+								<h2>..Loading</h2>
+							</>
+						)}
+					</Grid>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
